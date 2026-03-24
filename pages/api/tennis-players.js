132d26c70771257ebc18c023339692caa1d502dd
UnderReviewUR <jon.shepherd@myyahoo.com>
2026-03-24 11:01:07 -0500

    Update tennis-players.js

--- pages/api/tennis-players.js
+++ pages/api/tennis-players.js
@@ -1,1 +1,13 @@
+import surfaces from "../../data/tennis/surfaces";
+import atp from "../../data/tennis/atp";
+import wta from "../../data/tennis/wta";
 
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.status(200).json({
+    surfaces,
+    atp,
+    wta,
+  });
+}
