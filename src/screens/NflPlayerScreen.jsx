82d79f5858419887e518a8fd0b71035754ec3090
UnderReviewUR <jon.shepherd@myyahoo.com>
2026-04-01 10:34:57 -0500

    Create NflPlayerScreen.jsx

---
+++ src/screens/NflPlayerScreen.jsx
@@ -0,0 +1,10 @@
+export default function NflPlayerScreen() {
+  return (
+    <main className="screen">
+      <div style={{ padding: 24, textAlign: "center" }}>
+        <h2>NFL Player</h2>
+        <p>NFL player screen stub (R1 shell)</p>
+      </div>
+    </main>
+  );
+}
