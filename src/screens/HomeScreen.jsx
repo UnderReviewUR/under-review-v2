d0743006ad2052fb86671cbe8861fe421b88f715
UnderReviewUR <jon.shepherd@myyahoo.com>
2026-04-01 10:30:15 -0500

    Create HomeScreen.jsx - r1 shell

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+export default function HomeScreen() {
+  return (
+    <main className="screen">
+      <div style={{ padding: 24, textAlign: "center" }}>
+        <h2>Home</h2>
+        <p>Home screen stub (R1 shell)</p>
+      </div>
+    </main>
+  );
+}
