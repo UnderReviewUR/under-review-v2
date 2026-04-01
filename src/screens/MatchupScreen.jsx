fa2bf022eb847cc51b8c60d8b2940bf8717cc3e5
UnderReviewUR <jon.shepherd@myyahoo.com>
2026-04-01 10:33:56 -0500

    Create MatchupScreen.jsx - r1 shell

---
+++ src/screens/MatchupScreen.jsx
@@ -0,0 +1,10 @@
+export default function MatchupScreen() {
+  return (
+    <main className="screen">
+      <div style={{ padding: 24, textAlign: "center" }}>
+        <h2>Matchup</h2>
+        <p>Matchup screen stub (R1 shell)</p>
+      </div>
+    </main>
+  );
+}
